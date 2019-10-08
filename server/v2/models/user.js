0aba66c82704a8ccfe085c46cc9edfe59c93b2df
Sevenpros <sethbisoso@gmail.com>
2019-10-08 22:08:12 +0200

    feature(login):create - Create signin controller - create signin route [Finishes #169004927]

--- server/v2/models/user.js
+++ server/v2/models/user.js
@@ -3,6 +3,7 @@
 /* eslint-disable import/prefer-default-export */
 import uuid1 from 'uuidv1';
 import connectDb from './index';
+import Helper from '../../helpers/userHelper';
 
 
 class UserModel {
@@ -10,7 +11,7 @@ class UserModel {
     const query = {
       text: `INSERT INTO users (id, firstName, lastName, email, password, gender, job_role, department, address, is_admin)
         values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning *`,
-      values: [uuid1(), user.firstName, user.lastName, user.email, user.password, user.gender, user.jobRole, user.department, user.address, user.isadmin],
+      values: [uuid1(), user.firstName, user.lastName, user.email, Helper.securePassword(user.password), user.gender, user.jobRole, user.department, user.address, user.isadmin],
     };
     connectDb(query);
   }
